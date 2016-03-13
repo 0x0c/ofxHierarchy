8ec3105ff9fbb5f6d26ab6591aeac8fc4037ba86
Akira Matsuda <akira.matsuda@me.com>
2016-03-14 04:26:11 +0900

    initial commit

---
+++ src/ofxHierarchy.cpp
@@ -0,0 +1,78 @@
+//
+//  ofxHierarchy.cpp
+//  ofxHierarchy
+//
+//  Created by Akira Matsuda on 11/18/15.
+//
+//
+
+#include <functional>
+#include "ofxHierarchy.h"
+#include "ofGraphics.h"
+#include "ofBaseTypes.h"
+
+namespace ofxHierarchy {
+	View::View() : hidden(false), alpha(1), frame({0, 0, 0, 0}), rotation(0, 0, 0), color(ofColor::white), superview(nullptr), will_draw(nullptr), drawable_content(nullptr), did_draw(nullptr) {}
+	
+	View::View(ofxHierarchy::Rect frame, std::function<void(ofxHierarchy::Rect frame)> drawable_content) : hidden(false), alpha(1), color(ofColor::white), superview(nullptr), frame(frame), rotation(0, 0, 0), will_draw(nullptr), drawable_content(drawable_content), did_draw(nullptr) {
+	}
+	
+	void View::add_subview(ofxHierarchy::View *v) {
+		v->superview = this;
+		this->subviews.push_back(v);
+	}
+	
+	void View::draw() {
+		if (!this->hidden) {
+			if (this->will_draw) {
+				this->will_draw(this->frame);
+			}
+			
+			ofPushMatrix();
+			ofTranslate(this->frame.origin.x, this->frame.origin.y);
+			ofRotateX(this->rotation.x);
+			ofRotateY(this->rotation.y);
+			ofRotateZ(this->rotation.z);
+			ofSetColor(this->color.r, this->color.g, this->color.b, 255.0 * this->alpha);
+			if (this->drawable_content) {
+				this->drawable_content(this->frame);
+			}
+			for (auto view : this->subviews) {
+				view->draw();
+			}
+			ofPopMatrix();
+			
+			if (this->did_draw) {
+				this->did_draw(this->frame);
+			}
+		}
+	}
+	
+	void View::setCenterX(float x) {
+		this->frame.origin.x = x - this->frame.size.width / 2.0;
+	}
+	
+	void View::setCenterY(float y) {
+		this->frame.origin.y = y - this->frame.size.height / 2.0;
+	}
+	
+	void View::setCenter(float x, float y) {
+		this->setCenterX(x);
+		this->setCenterY(y);
+	}
+	
+	ofxHierarchy::Point View::center() {
+		ofxHierarchy::Point p = {static_cast<float>(this->frame.origin.x + this->frame.size.width / 2.0), static_cast<float>(this->frame.origin.y + this->frame.size.height / 2.0)};
+		
+		return p;
+	}
+	
+	bool View::hitTest(float x, float y) {
+		bool result = false;
+		if ((this->frame.origin.x < x && x < this->frame.origin.x + this->frame.size.width) && (this->frame.origin.y < y && y < this->frame.origin.y + this->frame.size.height)) {
+			result = true;
+		}
+		
+		return result;
+	}
+}
---
+++ src/ofxHierarchy.h
@@ -0,0 +1,65 @@
+//
+//  ofxHierarchy.h
+//  ofxHierarchy
+//
+//  Created by Akira Matsuda on 11/18/15.
+//
+//
+
+#ifndef ofxHierarchy_hpp
+#define ofxHierarchy_hpp
+
+#include <vector>
+#include <functional>
+#include "ofBaseTypes.h"
+#include "ofPoint.h"
+
+namespace ofxHierarchy {
+	struct Size {
+		float width;
+		float height;
+	};
+	
+	struct Point {
+		float x;
+		float y;
+	};
+	
+	struct Rect {
+		ofxHierarchy::Point origin;
+		ofxHierarchy::Size size;
+	};
+
+	class View {
+	public:
+		View();
+		View(ofxHierarchy::Rect frame, std::function<void(ofxHierarchy::Rect frame)> drawable_content);
+		~View() {};
+		bool hidden;
+		float alpha;
+		ofColor color;
+		ofVec3f rotation;
+		ofxHierarchy::Rect frame;
+		View *superview;
+		std::vector<ofxHierarchy::View *> subviews;
+		std::function<void(ofxHierarchy::Rect frame)> will_draw;
+		std::function<void(ofxHierarchy::Rect frame)> drawable_content;
+		std::function<void(ofxHierarchy::Rect frame)> did_draw;
+		
+		virtual void add_subview(ofxHierarchy::View *v);
+		virtual void draw();
+		void setCenterX(float x);
+		void setCenterY(float y);
+		void setCenter(float x, float y);
+		float getMaxX() {
+			return this->frame.origin.x + this->frame.size.width;
+		}
+		float getMaxY() {
+			return this->frame.origin.y + this->frame.size.height;
+		}
+		ofxHierarchy::Point center();
+		bool hitTest(float x, float y);
+	};
+}
+
+#endif /* ofxHierarchy_hpp */
